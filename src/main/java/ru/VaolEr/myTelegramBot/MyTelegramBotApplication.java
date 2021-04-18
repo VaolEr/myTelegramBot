4e26a788ca4265de99360bdda034b39ec7522c76
VaolEr <eremizin@gmail.com>
2021-04-18 13:02:06 +0300

    init commit

---
+++ src/main/java/ru/VaolEr/myTelegramBot/MyTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.VaolEr.myTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTelegramBotApplication.class, args);
+	}
+
+}
